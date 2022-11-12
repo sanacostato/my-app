621099d24da22be8e76a22277aff8272165564f1
sanacostato <saacosta.dwfsc9@ucamp.utel.edu.mx>
2022-11-11 20:04:33 -0500

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,83 +4,149 @@ import { NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
     <>
-    <nav className="navbar navbar-expand-lg bg-light">
+    <nav className="navbar navbar-dark bg-dark fixed-top">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">
-      Taller de recuperacion
-    </a>
+    
+    <NavLink className="navbar-brand" aria-current="page" to="/">
+                  HOME
+                </NavLink>
     <button
       className="navbar-toggler"
       type="button"
-      data-bs-toggle="collapse"
-      data-bs-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
+      data-bs-toggle="offcanvas"
+      data-bs-target="#offcanvasDarkNavbar"
+      aria-controls="offcanvasDarkNavbar"
     >
       <span className="navbar-toggler-icon" />
     </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <NavLink className="nav-link active" aria-current="page" to="/">
-            Home
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink className="nav-link" to="/crud">
-            CRUD
-          </NavLink>
-        </li>
-        <li className="nav-item dropdown">
-          <a
-            className="nav-link dropdown-toggle"
-            href="#"
-            role="button"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-          >
-            Dropdown
-          </a>
-          <ul className="dropdown-menu">
-            <li>
-              <a className="dropdown-item" href="#">
-                Action
-              </a>
-            </li>
-            <li>
-              <a className="dropdown-item" href="#">
-                Another action
-              </a>
-            </li>
-            <li>
-              <hr className="dropdown-divider" />
-            </li>
-            <li>
-              <a className="dropdown-item" href="#">
-                Something else here
-              </a>
-            </li>
-          </ul>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled">Disabled</a>
-        </li>
-      </ul>
-      <form className="d-flex" role="search">
-        <input
-          className="form-control me-2"
-          type="search"
-          placeholder="Search"
-          aria-label="Search"
+    <div
+      className="offcanvas offcanvas-end text-bg-dark"
+      tabIndex={-1}
+      id="offcanvasDarkNavbar"
+      aria-labelledby="offcanvasDarkNavbarLabel"
+    >
+      <div className="offcanvas-header">
+        <h5 className="offcanvas-title" id="offcanvasDarkNavbarLabel">
+          List
+        </h5>
+        <button
+          type="button"
+          className="btn-close btn-close-white"
+          data-bs-dismiss="offcanvas"
+          aria-label="Close"
         />
-        <button className="btn btn-outline-success" type="submit">
-          Search
-        </button>
-      </form>
+      </div>
+      <div className="offcanvas-body">
+        <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+          <li className="nav-item">
+            <NavLink   className="nav-link active" aria-current="page" href="/">
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" href="/menu">
+              Menu
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" href="/where">
+              Where
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" href="/booking">
+              Booking
+            </NavLink>
+          </li>
+          {/* <li className="nav-item dropdown">
+            <a
+              className="nav-link dropdown-toggle"
+              href="#"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Dropdown
+            </a>
+            <ul className="dropdown-menu dropdown-menu-dark">
+              <li>
+                <a className="dropdown-item" href="#">
+                  Action
+                </a>
+              </li>
+              <li>
+                <a className="dropdown-item" href="#">
+                  Another action
+                </a>
+              </li>
+              <li>
+                <hr className="dropdown-divider" />
+              </li>
+              <li>
+                <a className="dropdown-item" href="#">
+                  Something else here
+                </a>
+              </li>
+            </ul>
+          </li> */}
+        </ul>
+        {/* <form className="d-flex mt-3" role="search">
+          <input
+            className="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button className="btn btn-success" type="submit">
+            Search
+          </button>
+        </form> */}
+      </div>
     </div>
   </div>
 </nav>
+
+      {/* <nav className="navbar navbar-expand-lg bg-blue">
+        <div className="container-fluid">
+
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0 navbar-brand">
+              <li className="nav-item white">
+                <NavLink className="nav-link active white" aria-current="page" to="/">
+                  HOME
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/menu">
+                  MENU
+                </NavLink>
+              </li>
+              <li className="nav-item ">
+                <NavLink className="nav-link" to="/where">
+                  WHERE
+                </NavLink>
+              </li>
+              <li className="nav-item ">
+                <NavLink className="nav-link" to="/booking">
+                  BOOKING
+                </NavLink>
+              </li>
+             
+            </ul>
+          </div>
+        </div>
+      </nav> */}
 
     </>
   )
